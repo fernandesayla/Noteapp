53d918cc6a7716cc373c5c85835cc6722f143e80
ayla <fernandes.ayla@gmail.com>
2018-03-03 13:46:27 -0300

    App1 aula sabado

--- app/src/main/java/com/example/logonrm/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/noteapp/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.logonrm.noteapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MenuInflater
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +14,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.menu_salvar -> {
+
+                return true
+            }
+            R.id.menu_perquisar -> {
+
+                return true
+            }
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }
